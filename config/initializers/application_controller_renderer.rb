13881f8df633981debdd90da0e24e8c0ceffb2ca
kenmoi <ken.en001.mb@gmail.com>
2021-02-08 11:53:56 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0297a6e90dae42edf53d6d0361ad5b21e7a9e8ecccacdb258962a9454bf034e91895db7197e0b68947fcee75f6ad0ed79a6a3992ca0f92a67aacee8b1cd9298e'
